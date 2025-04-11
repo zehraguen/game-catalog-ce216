563581a6f8e6f7f07714fc1120fb54837ea9d4f5
Zehra Gün <90135499+zehraguen@users.noreply.github.com>
2025-04-11 13:19:03 +0300

    add classes into demo
    implemented
    - import json
    - export json

--- demo/src/main/java/com/example/demo/HelloApplication.java
+++ demo/src/main/java/com/example/demo/HelloApplication.java
@@ -13,7 +13,6 @@ import javafx.scene.layout.Priority;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 import javafx.scene.control.ListView;
-
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Random;
--- demo/src/main/java/module-info.java
+++ demo/src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module com.example.demo {
     requires javafx.controls;
     requires javafx.fxml;
+    requires org.json;
 
 
     opens com.example.demo to javafx.fxml;
