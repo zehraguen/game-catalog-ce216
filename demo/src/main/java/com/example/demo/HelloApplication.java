870bf981c534976f9eba9cd87ea39d7a5f72fb0d
ataGitAccount <atakemal07@hotmail.com>
2025-04-21 13:05:59 +0300

    GUI update
    The layout is redesigned. The buttons are disfunctional but ready to assign methods.

--- demo/src/main/java/com/example/demo/HelloApplication.java
+++ demo/src/main/java/com/example/demo/HelloApplication.java
@@ -5,82 +5,64 @@ import javafx.fxml.FXMLLoader;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Priority;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
-import javafx.scene.control.ListView;
+
 import java.io.IOException;
+import java.time.Year;
 import java.util.ArrayList;
 import java.util.Random;
 
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        /*
-        VBox vbox = new VBox();
-        HBox head = new HBox(8);
-        vbox.setAlignment(Pos.CENTER);
-
-        Label label = new Label("Information");
-        TextField tf = new TextField();
-        HBox.setHgrow(tf, Priority.ALWAYS);
-        Button benjamin = new Button("add");
-
-        ListView listView = new ListView<>();
-        VBox.setVgrow(listView, Priority.ALWAYS);
-
-        HBox end = new HBox(8);
-        end.setAlignment(Pos.CENTER_RIGHT);
-        Button button = new Button("OK");
-        end.getChildren().add(button);
-
-        VBox.setMargin(head, new Insets(8));
-        VBox.setMargin(listView, new Insets(8));
-        VBox.setMargin(end, new Insets(8));
-
-        head.getChildren().addAll(
-                label, tf, benjamin
-        );
-
-        vbox.getChildren().addAll(
-                head, listView, end
-        );
-
-        Scene scene = new Scene(vbox, 400,
-                300);
-        stage.setTitle("CE216 - Layouts");
-        stage.setScene(scene);
-        stage.show();
-         */
-
 
         VBox mainLayOut = new VBox();
         mainLayOut.setAlignment(Pos.TOP_CENTER);
 
         //-------------------------------------------------
+        Menu fileMenu = new Menu(("File"));
+        fileMenu.getItems().add(new MenuItem("Import"));
+        fileMenu.getItems().add(new MenuItem("Export"));
+        fileMenu.getItems().get(0).setOnAction(e -> System.out.println());
+        fileMenu.getItems().get(1).setOnAction(e -> System.out.println());
 
-        Button sortButton = new Button("Listed by:");
-        Label sortLabel = new Label("Alphabetical");
+        Menu filterGenre = new Menu("Genre");
+        filterGenre.getItems().add(new MenuItem(""));
+
+        Menu filterYear = new Menu(("Release Year"));
+        ToggleGroup tg = new ToggleGroup();
+        for(int i=2025; i>1989; i--){
+            filterYear.getItems().add(new RadioMenuItem("" + i));
+        }
+
+        Menu filterMenu = new Menu("Filter");
+        filterMenu.getItems().add(filterGenre);
+        filterMenu.getItems().add(filterYear);
+
+        MenuBar menuBar = new MenuBar();
+        menuBar.getMenus().add(fileMenu);
+        menuBar.getMenus().add(filterMenu);
+        menuBar.getMenus().add(new Menu("Help"));
+        mainLayOut.getChildren().add(menuBar);
+
+        Label sortLabel = new Label("Sorted by:");
+
+        ComboBox<String> sortBox = new ComboBox<>();
+        sortBox.getItems().addAll("Alphabetical", "Release year", "Play time");
+        sortBox.setValue("Alphabetical");
+        sortBox.setOnAction(e -> System.out.println());
+
         TextField searchField = new TextField();
-        searchField.setText("search");
+        searchField.setPromptText("search");
         searchField.setOnMouseClicked(e -> searchField.setText(""));
+        searchField.setMinHeight(30);
+
         Button searchButton = new Button("Search");
-
-        sortButton.setOnAction(e -> {
-            if (sortLabel.getText()=="Alphabetical") {
-                sortByYears();
-                sortLabel.setText("Release year");
-            }
-            else {
-                sortByAlphabeticalOrder();
-                sortLabel.setText("Alphabetical");
-            }
-        });
-
         searchButton.setOnAction(e -> {
             String s = searchField.getText();
             System.out.println(s);
@@ -88,49 +70,58 @@ public class HelloApplication extends Application {
 
         HBox head = new HBox(8);
         head.setAlignment(Pos.CENTER);
+        head.getChildren().addAll(sortLabel, sortBox, searchField, searchButton);
+        head.setMinSize(10, 40);
         HBox.setHgrow(searchField, Priority.ALWAYS);
-        head.getChildren().addAll( sortButton, sortLabel, searchField, searchButton);
 
         VBox.setMargin(head, new Insets(8));
 
         //-------------------------------------------------
 
-        Label filterLabel = new Label("None");
-        Button filterButton = new Button("   Filters:  ");
-        filterButton.setOnAction(e -> filterLabel.setText(filterLabel.getText() + " criteria"));
-
-        HBox filterBox = new HBox(8);
-        filterBox.setAlignment(Pos.CENTER_LEFT);
-        filterBox.getChildren().addAll(filterButton, filterLabel);
-
-        VBox.setMargin(filterBox, new Insets(8));
-        //-------------------------------------------------
-
         ListView gameView = new ListView();
         VBox.setVgrow(gameView, Priority.ALWAYS);
 
+        GridPane[] games = new GridPane[9];
+        for(int i=0; i<9; i++) {
+            games[i] = new GridPane();
+            games[i].add(new Label("game "+ i), 0, 0);
+        }
+
+        GridPane gp = new GridPane();
+        VBox.setVgrow(gp, Priority.ALWAYS);
+        gp.setAlignment(Pos.CENTER);
+        gp.setHgap(20);
+        gp.setVgap(16);
+
+        gp.add(games[0], 0, 0);
+        gp.add(games[1], 1, 0);
+        gp.add(games[2], 2, 0);
+        gp.add(games[3], 0, 1);
+        gp.add(games[4], 1, 1);
+        gp.add(games[5], 2,1);
+        gp.add(games[6], 0, 2);
+        gp.add(games[7], 1, 2);
+        gp.add(games[8], 2, 2);
+
         //-------------------------------------------------
 
-        Button helpButton = new Button("Help");
-        helpButton.setOnAction(e -> helpMenu());
         Button prevButton = new Button("prev");
         Button nextButton = new Button("next");
 
         HBox end = new HBox(8);
         end.setAlignment(Pos.BOTTOM_RIGHT);
-        end.getChildren().addAll(helpButton, prevButton, nextButton);
+        end.getChildren().addAll(prevButton, nextButton);
         VBox.setMargin(end, new Insets(8));
 
         //-------------------------------------------------
 
 
         mainLayOut.getChildren().add(head);
-        mainLayOut.getChildren().add(filterBox);
         mainLayOut.getChildren().add(gameView);
         mainLayOut.getChildren().add(end);
 
-        Scene scene = new Scene(mainLayOut, 400,
-                300);
+        Scene scene = new Scene(mainLayOut, 600,
+                450);
         stage.setTitle("Game Catalog");
         stage.setScene(scene);
         stage.show();
@@ -144,12 +135,6 @@ public class HelloApplication extends Application {
 
     public static void helpMenu() {
         System.out.println("This is the help menu.");
-    }
-    public static void sortByYears() {
-        System.out.println("Sorted by release years.");
-    }
-    public static void sortByAlphabeticalOrder() {
-        System.out.println("Sorted by alphabetical order.");
     }
     public  static String getSearchText(TextField tf) { return tf.getText(); }
 }
