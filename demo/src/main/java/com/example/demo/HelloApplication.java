d607e5b695c022fd75cde3741c981c7d1aeb3b4b
Zehra Gün <90135499+zehraguen@users.noreply.github.com>
2025-04-22 08:32:32 +0300

    Update HelloApplication.java

--- demo/src/main/java/com/example/demo/HelloApplication.java
+++ demo/src/main/java/com/example/demo/HelloApplication.java
@@ -1,13 +1,12 @@
 package com.example.demo;
 
 import javafx.application.Application;
-import javafx.collections.ObservableList;
-import javafx.fxml.FXMLLoader;
-import javafx.geometry.Insets;
-import javafx.geometry.Pos;
+import javafx.beans.property.SimpleDoubleProperty;
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.collections.FXCollections;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
-import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Priority;
 import javafx.scene.layout.VBox;
@@ -16,161 +15,98 @@ import javafx.stage.Stage;
 
 import java.io.File;
 import java.io.IOException;
-import java.time.Year;
 import java.util.ArrayList;
-import java.util.Random;
 
 public class HelloApplication extends Application {
     private final Catalog catalog = new Catalog();
-    private final ListView<String> gameView = new ListView<>();
+    private final TableView<Game> gameTable = new TableView<>();
 
     @Override
     public void start(Stage stage) throws IOException {
-
         VBox mainLayOut = new VBox();
-        mainLayOut.setAlignment(Pos.TOP_CENTER);
 
         //-------------------------------------------------
-        Menu fileMenu = new Menu(("File"));
-        fileMenu.getItems().add(new MenuItem("Import"));
-        fileMenu.getItems().add(new MenuItem("Export"));
-        fileMenu.getItems().get(0).setOnAction(e -> handleImportFromDirectory());
-        fileMenu.getItems().get(1).setOnAction(e -> exportGames());
-
-        Menu filterGenre = new Menu("Genre");
-        filterGenre.getItems().add(new MenuItem(""));
-
-        Menu filterYear = new Menu(("Release Year"));
-        ToggleGroup tg = new ToggleGroup();
-        for(int i=2025; i>1989; i--){
-            filterYear.getItems().add(new RadioMenuItem("" + i));
-        }
+        Menu fileMenu = new Menu("File");
+        MenuItem importItem = new MenuItem("Import");
+        MenuItem exportItem = new MenuItem("Export");
+        importItem.setOnAction(e -> handleImportFromDirectory());
+        exportItem.setOnAction(e -> exportGames());
+        fileMenu.getItems().addAll(importItem, exportItem);
 
         Menu filterMenu = new Menu("Filter");
-        filterMenu.getItems().add(filterGenre);
-        filterMenu.getItems().add(filterYear);
-
-        MenuBar menuBar = new MenuBar();
-        menuBar.getMenus().add(fileMenu);
-        menuBar.getMenus().add(filterMenu);
-        menuBar.getMenus().add(new Menu("Help"));
+        MenuBar menuBar = new MenuBar(fileMenu, filterMenu, new Menu("Help"));
         mainLayOut.getChildren().add(menuBar);
 
         Label sortLabel = new Label("Sorted by:");
-
         ComboBox<String> sortBox = new ComboBox<>();
         sortBox.getItems().addAll("Alphabetical", "Release year", "Play time");
         sortBox.setValue("Alphabetical");
-        //sortBox.setOnAction(e -> sortGames("alphabetical"));
         sortBox.setOnAction(e -> {
-            String selected = sortBox.getValue();
-            String criteria = switch (selected) {
+            String criteria = switch (sortBox.getValue()) {
                 case "Alphabetical" -> "alphabetical";
                 case "Release year" -> "chronological";
                 case "Play time" -> "playtime";
                 default -> "alphabetical";
             };
-
             catalog.sortGames(criteria);
-            gameView.setItems(getGameTitles()); // refresh the ListView
+            gameTable.setItems(FXCollections.observableArrayList(catalog.getGameList()));
         });
 
         TextField searchField = new TextField();
         searchField.setPromptText("search");
-        searchField.setOnMouseClicked(e -> searchField.setText(""));
         searchField.setMinHeight(30);
 
         Button searchButton = new Button("Search");
-        searchButton.setOnAction(e -> {
-            String s = searchField.getText();
-            System.out.println(s);
-        });
+        searchButton.setOnAction(e -> System.out.println(searchField.getText()));
 
-        HBox head = new HBox(8);
-        head.setAlignment(Pos.CENTER);
-        head.getChildren().addAll(sortLabel, sortBox, searchField, searchButton);
-        head.setMinSize(10, 40);
+        HBox head = new HBox(8, sortLabel, sortBox, searchField, searchButton);
+        head.setAlignment(javafx.geometry.Pos.CENTER);
         HBox.setHgrow(searchField, Priority.ALWAYS);
-
-        VBox.setMargin(head, new Insets(8));
-
-        //-------------------------------------------------
-
-        VBox.setVgrow(gameView, Priority.ALWAYS);
-
-        GridPane[] games = new GridPane[9];
-        for(int i=0; i<9; i++) {
-            games[i] = new GridPane();
-            games[i].add(new Label("game "+ i), 0, 0);
-        }
-
-        GridPane gp = new GridPane();
-        VBox.setVgrow(gp, Priority.ALWAYS);
-        gp.setAlignment(Pos.CENTER);
-        gp.setHgap(20);
-        gp.setVgap(16);
-
-        gp.add(games[0], 0, 0);
-        gp.add(games[1], 1, 0);
-        gp.add(games[2], 2, 0);
-        gp.add(games[3], 0, 1);
-        gp.add(games[4], 1, 1);
-        gp.add(games[5], 2,1);
-        gp.add(games[6], 0, 2);
-        gp.add(games[7], 1, 2);
-        gp.add(games[8], 2, 2);
+        VBox.setMargin(head, new javafx.geometry.Insets(8));
+        mainLayOut.getChildren().add(head);
 
         //-------------------------------------------------
 
-        Button prevButton = new Button("prev");
-        Button nextButton = new Button("next");
+        TableColumn<Game, String> titleCol = new TableColumn<>("Title");
+        titleCol.setCellValueFactory(data -> new SimpleStringProperty(data.getValue().getTitle()));
+
+        TableColumn<Game, Number> yearCol = new TableColumn<>("Year");
+        yearCol.setCellValueFactory(data -> new SimpleIntegerProperty(data.getValue().getReleaseYear()));
+
+        TableColumn<Game, Number> playtimeCol = new TableColumn<>("Playtime");
+        playtimeCol.setCellValueFactory(data -> new SimpleDoubleProperty(data.getValue().getPlayTime()));
+
+        TableColumn<Game, String> genreCol = new TableColumn<>("Genre");
+        genreCol.setCellValueFactory(data -> new SimpleStringProperty(String.join(", ", data.getValue().getGenre())));
+
+        gameTable.getColumns().addAll(titleCol, yearCol, playtimeCol, genreCol);
+        VBox.setVgrow(gameTable, Priority.ALWAYS);
+        mainLayOut.getChildren().add(gameTable);
 
         HBox end = new HBox(8);
-        end.setAlignment(Pos.BOTTOM_RIGHT);
-        end.getChildren().addAll(prevButton, nextButton);
-        VBox.setMargin(end, new Insets(8));
-
-        //-------------------------------------------------
-
-
-        mainLayOut.getChildren().add(head);
-        mainLayOut.getChildren().add(gameView);
+        end.setAlignment(javafx.geometry.Pos.BOTTOM_RIGHT);
+        end.getChildren().addAll(new Button("prev"), new Button("next"));
+        VBox.setMargin(end, new javafx.geometry.Insets(8));
         mainLayOut.getChildren().add(end);
 
-        Scene scene = new Scene(mainLayOut, 600,
-                450);
+        Scene scene = new Scene(mainLayOut, 600, 450);
         stage.setTitle("Game Catalog");
         stage.setScene(scene);
         stage.show();
-
     }
 
     public static void main(String[] args) {
         launch();
     }
 
-    public ObservableList<String> getGameTitles() {
-        ArrayList<String> titles = new ArrayList<>();
-        for (Game game : catalog.getGameList()) {
-            titles.add(game.getTitle());
-        }
-        return javafx.collections.FXCollections.observableArrayList(titles);
-    }
-
-    public void importGames() {
-        catalog.importJson("src/main/resources/games_data.json");
-        gameView.setItems(getGameTitles());
-    }
-
     public void handleImportFromDirectory() {
         FileChooser fileChooser = new FileChooser();
         fileChooser.setTitle("Import Game Data");
         fileChooser.getExtensionFilters().add(new FileChooser.ExtensionFilter("JSON Files", "*.json"));
-
         File file = fileChooser.showOpenDialog(null);
         if (file != null) {
             catalog.importJson(file.getAbsolutePath());
-            gameView.setItems(getGameTitles());
+            gameTable.setItems(FXCollections.observableArrayList(catalog.getGameList()));
         }
     }
 
@@ -179,24 +115,10 @@ public class HelloApplication extends Application {
         fileChooser.setTitle("Export Game Data");
         fileChooser.getExtensionFilters().add(new FileChooser.ExtensionFilter("JSON Files", "*.json"));
         fileChooser.setInitialFileName("games_export.json");
-
         File file = fileChooser.showSaveDialog(null);
         if (file != null) {
             catalog.exportJson(file.getAbsolutePath());
             System.out.println("Exported to: " + file.getAbsolutePath());
         }
     }
-
-
-
-    public void sortGames(String criteria) {
-        catalog.sortGames(criteria);
-        //gameView.setItems(getGameTitles());
-    }
-
-
-    public static void helpMenu() {
-        System.out.println("This is the help menu.");
-    }
-    public  static String getSearchText(TextField tf) { return tf.getText(); }
 }
