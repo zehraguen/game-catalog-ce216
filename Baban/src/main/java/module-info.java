9f49fc6458386e368c4512cbc320c03c67ede2ef
Dub26 <153666042+Dub26@users.noreply.github.com>
2025-04-23 15:52:49 +0300

    REAL VERY BIG UPDATE

--- Baban/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module com.example.baban {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires json;
-
-
-    opens com.example.baban to javafx.fxml;
-    exports com.example.baban;
-}
